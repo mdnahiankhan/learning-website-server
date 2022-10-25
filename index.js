fca0cd1512648191a827dd2dcef27b48c16c57a1
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-10-25 16:02:06 +0600

    added data

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const categories = require("./data/categories.json")
 
 app.get('/', (req, res) => {
     res.send('learning website is runing')
 })
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log(`learning website is running on port,${port}`);
